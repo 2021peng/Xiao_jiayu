3427af3566ad9ab63b4e45c718f3fb48193faaaa
2021peng <2285070166@qq.com>
2022-03-17 16:10:19 +0800

    L_035 wenjian

---
+++ Lesson_035_GUI/wenjian.py
@@ -0,0 +1,12 @@
+# 开发人：peng
+# 开发时间 ：2022/3/17 16:05
+import os
+import easygui as g
+
+file_path = g.fileopenbox(default="*.txt")
+
+with open(file_path) as f:
+    title = os.path.basename(file_path)
+    msg = "文件【%s】的内容如下："%title
+    text = f.read()
+    g.textbox(msg,title,text)
