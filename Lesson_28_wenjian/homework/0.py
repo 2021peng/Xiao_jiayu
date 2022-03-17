85a09d4c91be697a5d79ca3bd01823f6b6c78af6
2021peng <2285070166@qq.com>
2022-03-17 20:55:03 +0800

    L_028 first commit

---
+++ Lesson_28_wenjian/homework/0.py
@@ -0,0 +1,16 @@
+# 开发人：peng
+# 开发时间 ：2022/3/17 20:41
+
+def file_write(file_name):
+    f = open(file_name,'a+',encoding='utf-8')
+    print("请输入内容【按’:w‘保存退出！】:")
+    while True:
+        a = input()
+        if a != ':w':
+            f.write('%s\n'%a)
+        else:
+            break
+    f.close()
+
+file_name = input("请输入要新建的文件名：")
+file_write(file_name)
