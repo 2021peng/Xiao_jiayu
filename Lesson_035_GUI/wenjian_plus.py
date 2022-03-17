4ca05ca0201ca2b463d29981557471d4be05dbf7
2021peng <2285070166@qq.com>
2022-03-17 16:40:57 +0800

    L_035 wenjianplus

---
+++ Lesson_035_GUI/wenjian_plus.py
@@ -0,0 +1,28 @@
+# 开发人：peng
+# 开发时间 ：2022/3/17 16:19
+import os
+import easygui as g
+
+file_path = g.fileopenbox(default="*.txt")
+
+with open(file_path,encoding='utf-8') as old_file:
+    title = os.path.basename(file_path)
+    msg = "文件【%s】的内容如下："%title
+    text = old_file.read()
+    text_after = g.textbox(msg,title,text)
+
+if text != text_after[:-1]:
+    #textbox的返回值会追加一个换行符
+    choice = g.buttonbox("检测到文件的内容发生变化，请选择以下操作：","警告",("覆盖保存","放弃保存","另存为"))
+    if choice == "覆盖保存":
+        with open(file_path,'w+',encoding='utf-8') as old_file:
+            old_file.write(text_after[:-1])
+    if choice == "放弃":
+        pass
+    if choice == "另存为":
+        another_path = g.filesavebox(default=".txt")
+        if os.path.splitext(another_path)[1] != '.txt':
+            another_path += '.txt'
+        with open(another_path,'w+',encoding='utf-8') as new_file:
+            new_file.write(text_after[:-1])
+
