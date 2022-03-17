5b890ec7f7d2ff58a2c8deb2540dd142e0e62928
2021peng <2285070166@qq.com>
2022-03-17 16:17:46 +0800

    L_035 wenjian gbk

--- Lesson_035_GUI/wenjian.py
+++ Lesson_035_GUI/wenjian.py
@@ -1,11 +1,12 @@
 # 开发人：peng
 # 开发时间 ：2022/3/17 16:05
+
 import os
 import easygui as g
 
 file_path = g.fileopenbox(default="*.txt")
 
-with open(file_path) as f:
+with open(file_path,encoding='utf-8') as f:
     title = os.path.basename(file_path)
     msg = "文件【%s】的内容如下："%title
     text = f.read()
