f6b695391690c8dd22d437b4548fc48e3b9b6f75
2021peng <2285070166@qq.com>
2022-03-19 12:48:47 +0800

    L_030 video能够更改路径 commit2

--- Lesson_30_wenjianoperation/homework/wenjian_video.py
+++ Lesson_30_wenjianoperation/homework/wenjian_video.py
@@ -7,14 +7,15 @@
 """
 import os
 
-
+ext_lis = []
 def search_file(start_dir, target):
-    print(os.getcwd()+ '\nloop1')
+    #print(os.getcwd()+ '\nloop1')
     os.chdir(start_dir)
-    print(os.getcwd() + '\nloop1')
+    #print(os.getcwd() + '\nloop2')
 
     for each_file in os.listdir(os.curdir):
         ext = os.path.splitext(each_file)[1]
+        ext_lis.append(ext)
         if ext in target:
             vedio_list.append(os.getcwd() + os.sep + each_file + os.linesep)  # 使用os.sep是程序更标准
         if os.path.isdir(each_file):
@@ -22,7 +23,7 @@ def search_file(start_dir, target):
             os.chdir(os.pardir)  # 递归调用后切记返回上一层目录
 
 
-start_dir = input('请输入待查找的初始目录：')
+start_dir = input('请输入待查找的初始目录【如：C:/Users/admin/】：')
 program_dir = os.getcwd()
 
 target = ['.mp4', '.avi','.py','.rmvb']
@@ -33,4 +34,5 @@ search_file(start_dir, target)
 f = open(program_dir + os.sep + 'vedioList.txt', 'w')
 f.writelines(vedio_list)
 f.close()
+print(ext_lis)
 
